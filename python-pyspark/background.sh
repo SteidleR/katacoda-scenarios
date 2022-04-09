5088529ceaf5186a62282f1e4d37132e8e4ead21
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2022-04-09 12:49:40 +0200

    hadoop env variables

--- python-pyspark/background.sh
+++ python-pyspark/background.sh
@@ -6,6 +6,14 @@ tar xzf hadoop-3.2.3.tar.gz
 mv hadoop-3.2.3 hadoop
 
 export HADOOP_HOME=/root/hadoop
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
 
 python3 -m pip install pyspark
 python3 -m pip install pyspark[sql]
