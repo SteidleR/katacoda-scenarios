675da22041cce0b8f11c0b1c41acee7448cddabf
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2022-04-09 14:04:18 +0200

    hadoop installation

--- python-pyspark/background.sh
+++ python-pyspark/background.sh
@@ -2,7 +2,8 @@ apt install openjdk-8-jdk --assume-yes
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
 wget https://dlcdn.apache.org/hadoop/common/hadoop-3.2.3/hadoop-3.2.3.tar.gz
-tar -zxvf hadoop-3.2.3.tar.gz -C hadoop
+tar -zxvf hadoop-3.2.3.tar.gz
+mv hadoop-3.2.3 hadoop
 export HADOOP_HOME=/root/hadoop
 export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native
 
