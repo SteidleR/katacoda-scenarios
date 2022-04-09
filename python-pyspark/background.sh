03dbfc602f651e48a3128dd978cae222a7ec069b
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2022-04-09 14:00:00 +0200

    wip

--- python-pyspark/background.sh
+++ python-pyspark/background.sh
@@ -1,6 +1,11 @@
 apt install openjdk-8-jdk --assume-yes
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.2.3/hadoop-3.2.3.tar.gz
+tar -zxvf hadoop-3.2.3.tar.gz -C hadoop
+export HADOOP_HOME=/root/hadoop
+export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native
+
 wget https://dlcdn.apache.org/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
 tar -xvf spark-3.2.1-bin-hadoop3.2.tgz -C /usr/share
 ln -s /usr/share/spark-3.2.1-bin-hadoop3.2/ /usr/share/spark
