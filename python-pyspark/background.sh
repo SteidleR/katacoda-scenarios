e48b9ca1202a5db6961369ebd8ec0b3ac09452c6
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2022-04-09 13:29:27 +0200

    bg script

--- python-pyspark/background.sh
+++ python-pyspark/background.sh
@@ -3,9 +3,9 @@ export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
 wget https://www.apache.org/dyn/closer.lua/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
 tar -xvf spark-3.2.1-bin-hadoop3.2.tgz -C /usr/share
-ln -s /usr/share/spark-3.2.1-bin-hadoop3.2/ spark
+ln -s /usr/share/spark-3.2.1-bin-hadoop3.2/ /usr/share/spark
 echo "SPARK_HOME=/usr/share/spark" >> ~/.bashrc
-echo "PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bashrc" >> ~/.bashrc
+echo "PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin >> ~/.bashrc" >> ~/.bashrc
 source ~/.bashrc
 
 python3 -m pip install pyspark
