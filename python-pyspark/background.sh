5d9bc2aaa8bac93911d0794e07c9ab81987b0d4d
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2022-04-09 12:40:30 +0200

    fix hadoop url

--- python-pyspark/background.sh
+++ python-pyspark/background.sh
@@ -1,7 +1,7 @@
 apt install openjdk-8-jdk --assume-yes
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
-wget https://www.apache.org/dyn/closer.cgi/hadoop/common/hadoop-3.2.3/hadoop-3.2.3.tar.gz
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.2.3/hadoop-3.2.3.tar.gz
 tar xzf hadoop-3.2.3.tar.gz
 mv hadoop-3.2.3 hadoop
 
