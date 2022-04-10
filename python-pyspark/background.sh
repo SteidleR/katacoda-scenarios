95671c004273ad07c94582959ddc8523d88d1fb1
vap0r <inf19152@lehre.dhbw-stuttgart.de>
2022-04-10 12:18:18 +0200

    removing verbose tar

--- python-pyspark/background.sh
+++ python-pyspark/background.sh
@@ -2,11 +2,11 @@ apt install openjdk-8-jdk --assume-yes
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
 wget https://dlcdn.apache.org/hadoop/common/hadoop-3.2.3/hadoop-3.2.3.tar.gz
-tar -zxvf hadoop-3.2.3.tar.gz
+tar -zxf hadoop-3.2.3.tar.gz
 mv hadoop-3.2.3 hadoop
 
 wget https://dlcdn.apache.org/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
-tar -xvf spark-3.2.1-bin-hadoop3.2.tgz -C /usr/share
+tar -xf spark-3.2.1-bin-hadoop3.2.tgz -C /usr/share
 ln -s /usr/share/spark-3.2.1-bin-hadoop3.2/ /usr/share/spark
 echo "SPARK_HOME=/usr/share/spark" >> ~/.bashrc
 echo "PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin >> ~/.bashrc" >> ~/.bashrc
